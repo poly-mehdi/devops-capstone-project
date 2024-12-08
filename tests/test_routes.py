826f3297ebec68abeee1663afffc0909790311e3
poly-mehdi <mehdi.sehad@polymtl.ca>
2024-12-08 09:29:21 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -150,7 +150,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-    
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
